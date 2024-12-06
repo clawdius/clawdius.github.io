dd1d9609ed67b22804a89f7224b90913323005e1
Aditya Lila <aditapik@gmail.com>
2024-12-06 07:25:52 +0700

    enhance: prettier things

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let pairContent = [
-    ['twitter.com/', 'clawdius_', 'Twitter', 'This is the place where most of my sanity drains.', 'Visit My Twitter', 'https://twitter.com/clawdius_', 'blue'],
-    ['instagram.com/', 'clawdius_', 'Instagram', 'I rarely upload photos but I love to spam instastory.', 'Visit My Instagram', 'https://instagram.com/clawdius_', 'orange'],
-    ['steamcommunity.com/id/', 'clawdius_', 'Steam', 'From Rainbow Six: Siege to Stardew Valley, I enjoy wide variety of games.', 'Visit My Steam', 'https://steamcommunity.com/id/clawdius_', 'grey'],
-    ['facebook.com/', 'clawdiusunderscore', 'Facebook', "Who uses facebook these days? That's right, it's me.", 'Visit My Facebook', 'https://facebook.com/clawdiusunderscore', 'dodger-blue'],
-    ['pinterest.com/', 'clawdius_', 'Pinterest', 'If others use Behance to post their digital portofolio, then i use Pinterest.', 'Visit My Pinterest', 'https://pinterest.com/clawdius_/my-finest-creation/', 'firebrick'],
-    ['youtube.com/', '@clawdius_', 'Youtube', 'I mostly upload my 3D animation contents here, instead of Pinterest.', 'Visit My Youtube', 'https://youtube.com/@clawdius_', 'red'],
-    ['github.com/', 'clawdius', 'Github', 'Thank you Github for hosting this static website for free.', 'Visit My Github', 'https://github.com/clawdius', 'black-grey'],
-    ['mailto:', 'adit@thecode.id', 'Email', 'Have any question or something you want to say?', 'Mail Me', 'mailto:adit@thecode.id', 'purple']
-]
+    ["twitter.com/", "clawdius_", "Twitter", "This is the place where most of my sanity drains.", "Visit My Twitter", "https://twitter.com/clawdius_", "blue"],
+    ["instagram.com/", "clawdius_", "Instagram", "I rarely upload photos but I love to spam instastory.", "Visit My Instagram", "https://instagram.com/clawdius_", "orange"],
+    ["steamcommunity.com/id/", "clawdius_", "Steam", "From Rainbow Six: Siege to Stardew Valley, I enjoy wide variety of games.", "Visit My Steam", "https://steamcommunity.com/id/clawdius_", "grey"],
+    ["facebook.com/", "clawdiusunderscore", "Facebook", "Who uses facebook these days? That's right, it's me.", "Visit My Facebook", "https://facebook.com/clawdiusunderscore", "dodger-blue"],
+    ["pinterest.com/", "clawdius_", "Pinterest", "If others use Behance to post their digital portofolio, then i use Pinterest.", "Visit My Pinterest", "https://pinterest.com/clawdius_/my-finest-creation/", "firebrick"],
+    ["youtube.com/", "@clawdius_", "Youtube", "I mostly upload my 3D animation contents here, instead of Pinterest.", "Visit My Youtube", "https://youtube.com/@clawdius_", "red"],
+    ["github.com/", "clawdius", "Github", "Thank you Github for hosting this static website for free.", "Visit My Github", "https://github.com/clawdius", "black-grey"],
+    ["mailto:", "adit@thecode.id", "Email", "Have any question or something you want to say?", "Mail Me", "mailto:adit@thecode.id", "purple"],
+];
 
-let mainContent = document.getElementById('main-content').children;
-let logoList = document.getElementById('logo-list').children;
+let mainContent = document.getElementById("main-content").children;
+let logoList = document.getElementById("logo-list").children;
 let ornaments = document.querySelectorAll(".ornament");
-let clockH = document.getElementById('clockH');
-let clockM = document.getElementById('clockM');
-let clockS = document.getElementById('clockS');
-let signature = document.getElementById('signature');
-let overlay = document.getElementById('overlay');
+let clockH = document.getElementById("clockH");
+let clockM = document.getElementById("clockM");
+let clockS = document.getElementById("clockS");
+let signature = document.getElementById("signature");
+let overlay = document.getElementById("overlay");
 
 function startTime() {
     const today = new Date();
@@ -36,22 +36,22 @@ function startTime() {
 
 function checkTime(i) {
     if (i < 10) {
-        i = "0" + i
-    };
+        i = "0" + i;
+    }
     return i;
 }
 
 function changeLogoState(index) {
-    let logo = document.getElementsByClassName('logo-list')
+    let logo = document.getElementsByClassName("logo-list");
     for (let i = 0; i < logo.length; i++) {
-        logo[i].style.opacity = '50%'
-        logo[i].style.backgroundColor = null
+        logo[i].style.opacity = "50%";
+        logo[i].style.backgroundColor = null;
 
-        if(i == logo.length - 1) {
-            logo[index].style.opacity = '1'
-            logo[index].style.backgroundColor = 'var(--' + pairContent[index][6] + ')'
+        if (i == logo.length - 1) {
+            logo[index].style.opacity = "1";
+            logo[index].style.backgroundColor = "var(--" + pairContent[index][6] + ")";
         }
-    }  
+    }
 }
 
 function transitionList() {
@@ -59,8 +59,8 @@ function transitionList() {
     for (let i = 0; i < logoList.length; i++) {
         logoList[i].style.animation = `appearBT-logo 0.8s ease-in-out ${delay}s`;
         logoList[i].onanimationend = () => {
-            logoList[i].style.opacity = '50%'
-        }
+            logoList[i].style.opacity = "50%";
+        };
         delay += 0.075;
     }
 }
@@ -70,16 +70,16 @@ function transitionEnter() {
     for (let i = 0; i < mainContent.length; i++) {
         mainContent[i].style.animation = `appearBT 0.35s ease-in-out ${delay}s`;
         mainContent[i].onanimationend = () => {
-            mainContent[i].style.opacity = '1'
-        }
+            mainContent[i].style.opacity = "1";
+        };
         delay += 0.2;
     }
 
-    setTimeout(function() {
+    setTimeout(function () {
         for (let i = 0; i < logoList.length; i++) {
-            logoList[i].setAttribute('onclick', `changeMainContent(${i})`);
+            logoList[i].setAttribute("onclick", `changeMainContent(${i})`);
         }
-    }, 1000)
+    }, 1000);
 }
 
 function transitionQuit() {
@@ -87,53 +87,52 @@ function transitionQuit() {
     for (let i = 0; i < mainContent.length; i++) {
         mainContent[i].style.animation = `disappearBT 0.35s ease-in-out ${delay}s`;
         mainContent[i].onanimationend = () => {
-            mainContent[i].style.opacity = '0'
-        }
+            mainContent[i].style.opacity = "0";
+        };
         delay += 0.15;
     }
 
     for (let i = 0; i < logoList.length; i++) {
-        logoList[i].setAttribute('onclick', '');
+        logoList[i].setAttribute("onclick", "");
     }
 }
 
 function ornamentIn() {
     let delay = 1;
-    for(let i = 0; i < ornaments.length; i++) {
-        ornaments[i].style.animation = `ornamentsFadeIn 1s ease-in-out ${delay}s `
+    for (let i = 0; i < ornaments.length; i++) {
+        ornaments[i].style.animation = `ornamentsFadeIn 1s ease-in-out ${delay}s `;
         ornaments[i].onanimationend = () => {
-            ornaments[i].classList.remove("ornament")
-        }
+            ornaments[i].classList.remove("ornament");
+        };
         delay += 0.25;
     }
 }
 
 function changeMainContent(index) {
-    changeLogoState(index)
-    transitionQuit()
-    setTimeout(function() {
+    changeLogoState(index);
+    transitionQuit();
+    setTimeout(function () {
         onanimationend = () => {
-           setInnerContent(index)
-        }
-    }, 800)
-    setTimeout(transitionEnter, 1000)
-
+            setInnerContent(index);
+        };
+    }, 800);
+    setTimeout(transitionEnter, 1000);
 }
 
 function setInnerContent(index) {
-    document.getElementById('text-link').innerText = pairContent[index][0]
-    document.getElementById('text-username').innerHTML = pairContent[index][1]
-    document.getElementById('media-name').innerHTML = pairContent[index][2]
-    document.getElementById('media-desc').innerHTML = pairContent[index][3]
-    document.getElementById('media-button').innerHTML = pairContent[index][4]
-    document.getElementById('media-link').href = pairContent[index][5]
+    document.getElementById("text-link").innerText = pairContent[index][0];
+    document.getElementById("text-username").innerHTML = pairContent[index][1];
+    document.getElementById("media-name").innerHTML = pairContent[index][2];
+    document.getElementById("media-desc").innerHTML = pairContent[index][3];
+    document.getElementById("media-button").innerHTML = pairContent[index][4];
+    document.getElementById("media-link").href = pairContent[index][5];
 }
 
 function setSignature() {
-    let name = ""
+    let name = "";
 
     // Funsies Easter Egg
     name = Math.random() > 0.75 ? "Pemuja Karina" : "Aditya Lila";
-    
-    signature.innerHTML = `${name}. <span class="fw-bold">${new Date().getFullYear()}</span>`
+
+    signature.innerHTML = `${name}. <span class="fw-bold">${new Date().getFullYear()}</span>`;
 }
--- unfollowers/script.js
+++ unfollowers/script.js
@@ -20,100 +20,97 @@ buttonCompare.style.opacity = "0";
 resultCP.style.opacity = "0";
 
 (() => {
-  // Reader helper
-  const reader = (fileInput) => {
-    return new Promise((resolve) => {
-      const file = fileInput.files[0];
+    // Reader helper
+    const reader = (fileInput) => {
+        return new Promise((resolve) => {
+            const file = fileInput.files[0];
 
-      if (!file) {
-        resolve(alert(`Please insert the "${fileInput.name}" file first`));
-        return;
-      }
+            if (!file) {
+                resolve(alert(`Please insert the "${fileInput.name}" file first`));
+                return;
+            }
 
-      const fileReader = new FileReader();
+            const fileReader = new FileReader();
 
-      fileReader.onload = () => {
-        const res = JSON.parse(fileReader.result);
-        resolve(res);
-      };
+            fileReader.onload = () => {
+                const res = JSON.parse(fileReader.result);
+                resolve(res);
+            };
 
-      fileReader.readAsText(file);
-    });
-  };
+            fileReader.readAsText(file);
+        });
+    };
 
-  function compare(following, followers) {
-    if (!following || !followers) return;
+    function compare(following, followers) {
+        if (!following || !followers) return;
 
-    if (!following.relationships_following)
-      return alert(`"following" file is in incorrect format`);
-    if (!followers[0]) return alert(`"followers" file is in incorrect format`);
+        if (!following.relationships_following) return alert(`"following" file is in incorrect format`);
+        if (!followers[0]) return alert(`"followers" file is in incorrect format`);
 
-    let mismatch = [];
+        let mismatch = [];
 
-    following.relationships_following.forEach((d) => {
-      let dup = false;
-      followers.forEach((e) => {
-        d.string_list_data[0].value == e.string_list_data[0].value
-          ? (dup = true)
-          : null;
-      });
-      dup
-        ? null
-        : mismatch.push({
-            name: d.string_list_data[0].value,
-            link: d.string_list_data[0].href,
-          });
-    });
+        following.relationships_following.forEach((d) => {
+            let dup = false;
+            followers.forEach((e) => {
+                d.string_list_data[0].value == e.string_list_data[0].value ? (dup = true) : null;
+            });
+            dup
+                ? null
+                : mismatch.push({
+                      name: d.string_list_data[0].value,
+                      link: d.string_list_data[0].href,
+                  });
+        });
 
-    return mismatch;
-  }
-
-  buttonCompare.addEventListener("click", async () => {
-    const result = compare(await reader(following), await reader(followers));
-
-    if (result) {
-      resultDiv.innerHTML = "";
-
-      resultCP.classList.remove("d-md-block");
-      resultCP.classList.add("d-none");
-
-      resultC.classList.remove("d-none");
-      resultC.classList.add("d-md-block");
-
-      let c = 1;
-      let rc = 0;
-      let row;
-      result.forEach((d, index) => {
-        if (rc === 0) {
-          row = document.createElement("div");
-          row.classList = "row w-100";
-        }
-
-        const div = document.createElement("div");
-        div.classList = "result-content col-12 col-md-3";
-        div.innerHTML = `<a href=${d.link} target="_blank">${c}. ${d.name}</a>`;
-        row.append(div);
-        c += 1;
-        rc += 1;
-
-        if (rc === 4 || index === result.length - 1) {
-          resultDiv.append(row);
-          rc = 0;
-        }
-      });
+        return mismatch;
     }
-  });
+
+    buttonCompare.addEventListener("click", async () => {
+        const result = compare(await reader(following), await reader(followers));
+
+        if (result) {
+            resultDiv.innerHTML = "";
+
+            resultCP.classList.remove("d-md-block");
+            resultCP.classList.add("d-none");
+
+            resultC.classList.remove("d-none");
+            resultC.classList.add("d-md-block");
+
+            let c = 1;
+            let rc = 0;
+            let row;
+            result.forEach((d, index) => {
+                if (rc === 0) {
+                    row = document.createElement("div");
+                    row.classList = "row w-100";
+                }
+
+                const div = document.createElement("div");
+                div.classList = "result-content col-12 col-md-3";
+                div.innerHTML = `<a href=${d.link} target="_blank">${c}. ${d.name}</a>`;
+                row.append(div);
+                c += 1;
+                rc += 1;
+
+                if (rc === 4 || index === result.length - 1) {
+                    resultDiv.append(row);
+                    rc = 0;
+                }
+            });
+        }
+    });
 })();
 
 function firstEnter() {
-  let delay = 0.1;
-  let appearList = [title, followingC, followersC, buttonCompare, resultCP];
+    let delay = 0.1;
+    let appearList = [title, followingC, followersC, buttonCompare, resultCP];
 
-  for (const d of appearList) {
-    d.style.animation = `appearBT 0.35s ease-in-out ${delay}s`;
-    d.onanimationend = () => {
-      d.style.opacity = "1";
-    };
-    delay += 0.2;
-  }
+    for (const d of appearList) {
+        d.style.animation = `appearBT 0.35s ease-in-out ${delay}s`;
+        d.onanimationend = () => {
+            d.style.opacity = "1";
+        };
+        delay += 0.2;
+    }
 }

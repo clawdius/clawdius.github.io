343bce75ae9f7230129f6451f37f916cda700070
Aditya Lila <aditapik@gmail.com>
2025-06-05 19:25:46 +0700

    feat: Added password gambling app
    GOLD GOLD GOLD

---
+++ password-generator/main.js
@@ -0,0 +1,149 @@
+const caseConts = document.querySelector("#case-container");
+const caseWrap = document.querySelector("#container-wrapper");
+const baseOffset = -4870;
+
+const overlay = document.querySelector("#gamble-overlay");
+const pw = document.querySelector("#pw");
+
+// Bypass pesky "change" event for pw field because we add it via JS
+const pwChange = new Event("change");
+
+const innerCase = (r, c) => {
+    return `
+        <div class="${r} inner m-1">${c}</div>
+    `;
+};
+
+function generate(val) {
+    let r = 0;
+    let e = [];
+
+    const iBlue = "abcdefghijklmnopqrstuvwxyz";
+    const iPurple = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const iPink = "1234567890";
+    const iRed = "!@#$%^&*()";
+    const iGold = "[];',./";
+
+    for (let i = 0; i < val; i++) {
+        r = Math.random();
+
+        if (r < 0.0026) {
+            e.push({
+                r: "gold",
+                c: iGold.charAt(Math.floor(Math.random() * iGold.length)),
+            });
+            continue;
+        }
+        if (r < 0.0064) {
+            e.push({
+                r: "red",
+                c: iRed.charAt(Math.floor(Math.random() * iRed.length)),
+            });
+            continue;
+        }
+        if (r < 0.032) {
+            e.push({
+                r: "pink",
+                c: iPink.charAt(Math.floor(Math.random() * iPink.length)),
+            });
+            continue;
+        }
+        if (r < 0.1598) {
+            e.push({
+                r: "purple",
+                c: iPurple.charAt(Math.floor(Math.random() * iPurple.length)),
+            });
+            continue;
+        }
+
+        e.push({
+            r: "blue",
+            c: iBlue.charAt(Math.floor(Math.random() * iBlue.length)),
+        });
+    }
+
+    return e;
+}
+
+function open() {
+    const sNormal = new Audio("media/case normal.mp3");
+    const sGold = new Audio("media/case gold.mp3");
+
+    sNormal.oncanplay = () => {
+        overlay.classList.remove("hidden");
+        overlay.style.opacity = 0;
+        overlay.animate([{ opacity: 0 }, { opacity: 1 }], 100).onfinish = () => {
+            overlay.style.opacity = 1;
+        };
+
+        caseConts.innerHTML = "";
+
+        const g = generate(70);
+        let s = ``;
+
+        for (let j of g) {
+            s += `${innerCase(j.r, j.c)} `;
+        }
+
+        const xOffset = Math.floor(Math.random() * (40 + 40 + 1) - 40);
+        const wOffset = (caseWrap.clientWidth - 1536) / 2;
+
+        caseConts.innerHTML = s;
+        let spin = caseConts.animate([{ transform: "translateX(0px)" }, { transform: `translateX(${baseOffset + wOffset + xOffset}px)` }], {
+            duration: 5200,
+            easing: "ease-out",
+        });
+
+        if (g[57].r === "gold") {
+            sGold.play();
+        } else {
+            sNormal.play();
+        }
+
+        spin.onfinish = () => {
+            caseConts.style.transform = `translateX(${baseOffset + wOffset + xOffset}px)`;
+            setTimeout(() => {
+                overlay.animate([{ opacity: 1 }, { opacity: 0 }], 200).onfinish = () => {
+                    overlay.classList.add("hidden");
+                    pw.dispatchEvent(pwChange);
+                };
+                pw.value = `${pw.value}${g[57].c}`;
+            }, 1000);
+        };
+    };
+}
+
+window.onload = () => {
+    const gambleButton = document.querySelector("#gambleButton");
+    const copyButton = document.querySelector("#copyToClipboard");
+    const resetButton = document.querySelector("#resetButton");
+
+    copyButton.addEventListener("click", () => {
+        if (pw.value != "") {
+            pw.select();
+            pw.setSelectionRange(0, 9999);
+
+            navigator.clipboard.writeText(pw.value);
+            alert("Copied to clipboard!");
+        } else {
+            alert("You haven't gamble yet")
+        }
+    });
+
+    pw.addEventListener("change", () => {
+        if (pw.value != "") {
+            resetButton.classList.remove("hidden");
+        } else {
+            resetButton.classList.add("hidden");
+        }
+    });
+
+    resetButton.addEventListener("click", () => {
+        pw.value = "";
+        pw.dispatchEvent(pwChange);
+    });
+
+    gambleButton.addEventListener("click", () => {
+        open();
+    });
+};
